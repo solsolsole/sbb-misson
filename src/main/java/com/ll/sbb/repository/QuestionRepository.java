75020ce5844c64bdd3ba9890f55fec3a8af11206
solsolsole <dmsthf917@naver.com>
2023-11-21 20:19:53 +0900

    chap2-05

--- src/main/java/com/ll/sbb/repository/QuestionRepository.java
+++ src/main/java/com/ll/sbb/repository/QuestionRepository.java
@@ -3,6 +3,10 @@ package com.ll.sbb.repository;
 import com.ll.sbb.entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
+import java.util.List;
 
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String Content);
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -1,12 +1,21 @@
 package com.ll.sbb;
 
+import com.ll.sbb.entity.Answer;
 import com.ll.sbb.entity.Question;
+import com.ll.sbb.repository.AnswerRepository;
 import com.ll.sbb.repository.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalLong;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class SbbApplicationTests {
@@ -14,18 +23,21 @@ class SbbApplicationTests {
     @Autowired
     private QuestionRepository questionRepository;
 
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Transactional  // 메서드가 종료될때 까지 DB세션 유지
     @Test
     void testJpa() {
-        Question q1 = new Question();
-        q1.setSubject("sbb가 무엇인가요?");
-        q1.setContent("sbb에 대해서 알고 싶습니다.");
-        q1.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 첫번째 질문 저장
+        Optional<Question> oq =this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
 
-        Question q2 = new Question();
-        q2.setSubject("스프링부트 질문입니다.");
-        q2.setContent("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q1);  // 두번째 질문 저장
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
     }
 }
+
+

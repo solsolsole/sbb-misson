48ef0d0899f13aedb8346668b12e75e13c7ca730
solsolsole <dmsthf917@naver.com>
2023-11-22 10:49:57 +0900

    chap2-10

--- src/main/java/com/ll/sbb/service/QuestionService.java
+++ src/main/java/com/ll/sbb/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.ll.sbb.service;
 
 import com.ll.sbb.entity.Question;
+import com.ll.sbb.golobal.DataNotFoundException;
 import com.ll.sbb.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +17,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()){
+            return question.get();
+        } else {
+            throw new DataNotFoundException("quetion not found");
+        }
     }
 }

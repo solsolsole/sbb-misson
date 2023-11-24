eecb521dbb25e0c9ff12786eb143fd8238510fb8
solsolsole <dmsthf917@naver.com>
2023-11-24 17:08:18 +0900

    chap3-07

--- src/main/java/com/ll/sbb/user/SecurityConfig.java
+++ src/main/java/com/ll/sbb/user/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.ll.sbb.user;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,9 +22,16 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
                 .csrf((csrf) -> csrf  // h2 콘솔은 일반 어플리케이션 스프링 기능이 없게 때문에 따로 허용하는 걸 만들어줌
                         .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
-                .headers((headers) -> headers
+                .headers((headers) -> headers  // h2 db와 프레임 맞춤
                         .addHeaderWriter(new XFrameOptionsHeaderWriter(
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin  // 로그인
+                        .loginPage("/user/login")
+                        .defaultSuccessUrl("/"))
+                .logout((logout) -> logout          // 로그아웃
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
+                        .logoutSuccessUrl("/")
+                        .invalidateHttpSession(true))
         ;
         return http.build();
     }
@@ -30,5 +39,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration)
+        throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/ll/sbb/user/user/controller/UserController.java
+++ src/main/java/com/ll/sbb/user/user/controller/UserController.java
@@ -40,12 +40,16 @@ public class UserController {
             e.printStackTrace();
             bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
             return "signup_form";
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
             bindingResult.reject("signupFailed", e.getMessage());
             return "signup_form";
         }
         return "redirect:/";
+    }
+
+    @GetMapping("/login")
+    public String login () {
+        return "login_form";
     }
 }
